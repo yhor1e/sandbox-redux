d36c77d3a2b99a6a7842714e68cf6d93d83e9d14
yhorie <yuk.horie@gmail.com>
2016-12-17 23:34:41 +0900

    add react counter

---
+++ 2/main.js
@@ -0,0 +1,18 @@
+const createStore = require('redux').createStore,
+      React = require('react'),
+      ReactDOM = require('react-dom'),
+      Counter = require('./components/counter'),
+      counter = require('./reducers/counter'),
+      store = createStore(counter);
+
+const render = () => ReactDOM.render(
+    <Counter
+  value={store.getState()}
+  onIncrement={() => store.dispatch({ type: 'INCREMENT'})}
+  onDecrement={() => store.dispatch({ type: 'DECREMENT'})}
+    />,
+  document.getElementById('root')
+)
+
+render()
+store.subscribe(render);
--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # sandbox-redux
 
-## 1
+## 1 vanilla counter
 
 ref: https://github.com/reactjs/redux/blob/master/examples/counter-vanilla/index.html
+
+## 2 react counter
+
+ref: https://github.com/reactjs/redux/tree/master/examples/counter
